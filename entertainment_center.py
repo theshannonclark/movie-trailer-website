1b2bdcb0c90b031569212c8eb2bb7123f030aae4
Shannon Clark <theshannonclark@gmail.com>
2015-11-30 18:13:50 +1100

    Create entertainment_center.py
    This module initialises a list of movies, and uses the fresh_tomatoes open_movies_page
    function to output the movies' data to an HTML document, and displays that document
    in the user's web browser.

---
+++ entertainment_center.py
@@ -0,0 +1,26 @@
+import media
+import lib.fresh_tomatoes as ft
+
+# Store movie data
+# todo: update to use xml
+movies = [
+    media.Movie("Melancholia",
+                "http://ia.media-imdb.com/images/M/MV5BMTk4NjM0MjI3MV5BMl5BanBnXkFtZTcwNjcxMDYzNg@@._V1_SX214_AL_.jpg",
+                "https://www.youtube.com/watch?v=wzD0U841LRM"),
+
+    media.Movie("The Matrix",
+                "http://ia.media-imdb.com/images/M/MV5BMTkxNDYxOTA4M15BMl5BanBnXkFtZTgwNTk0NzQxMTE@._V1_SX214_AL_.jpg",
+                "https://www.youtube.com/watch?v=m8e-FF8MsqU"),
+
+    media.Movie("No Country for Old Men",
+                "http://ia.media-imdb.com/images/M/MV5BMjA5Njk3MjM4OV5BMl5BanBnXkFtZTcwMTc5MTE1MQ@@._V1_SY317_CR0,0,214,317_AL_.jpg",
+                "https://www.youtube.com/watch?v=YOohAwZOSGo"),
+
+    media.Movie("The Fountain",
+                "http://ia.media-imdb.com/images/M/MV5BMTU5OTczMTcxMV5BMl5BanBnXkFtZTcwNDg3MTEzMw@@._V1_SY317_CR0,0,214,317_AL_.jpg",
+                "https://www.youtube.com/watch?v=dAuxryJ6pv8")
+    ]
+
+# Write movies out to a html document
+# and display it in the browser.
+ft.open_movies_page(movies)
