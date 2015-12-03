d081147b339b54ad36c2f2a76fce641a9d0d48c4
Shannon Clark <theshannonclark@gmail.com>
2015-12-03 17:21:22 +1100

    Fix minor errors
    I forgot to write media.MovieEssence instead of just MovieEssence, and I wasn't
    aware that python methods need to take a self argument.

--- media.py
+++ media.py
@@ -7,33 +7,33 @@ class MovieEssence():
         self.poster_image_url = ""
         self.trailer_youtube_url = ""
 
-    def set_title(new_title):
+    def set_title(self, new_title):
         self.title = new_title
         return self
 
-    def set_poster_image_url(poster_image_url):
+    def set_poster_image_url(self, poster_image_url):
         self.poster_image_url = poster_image_url
         return self
 
-    def set_trailer_youtube_url(youtube_url):
+    def set_trailer_youtube_url(self, youtube_url):
         self.trailer_youtube_url = youtube_url
         return self
 
-    def is_valid():
+    def is_valid(self):
         result = True
-        if !is_valid_string(self.title):
+        if not self.is_valid_string(self.title):
             result = False
-        if !is_valid_string(self.poster_image_url):
+        if not self.is_valid_string(self.poster_image_url):
             result = False
-        if !is_valid_string(self.trailer_youtube_url):
+        if not self.is_valid_string(self.trailer_youtube_url):
             result = False
 
         return result
 
-    def is_valid_string(string):
+    def is_valid_string(self, string):
         return (type(string) is str) and (len(string) > 0)
 
-    def get_movie():
+    def get_movie(self):
         if self.is_valid():
             return Movie(self)
 
