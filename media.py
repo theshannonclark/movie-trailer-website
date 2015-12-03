0b6bff1bfc59899709c3350309f076181835edf7
Shannon Clark <theshannonclark@gmail.com>
2015-12-03 17:26:50 +1100

    Merge from movie-essence branch
    Using the essence design pattern so that I can add extra data to the Movie class
    without creating an extremely long parameter list.
    
    See the movie-essence branch for extra details.
    
    Squashed commit of the following:
    
    d081147 Fix minor errors
    17c8a50 Update entertainment_center.py to use MovieEssence
    5beb3a9 Validate MovieEssence and instantiate Movie
    8a99598 Create MovieEssence class

--- media.py
+++ media.py
@@ -1,9 +1,46 @@
 import lib.fresh_tomatoes as ft
 
-class Movie():
+class MovieEssence():
 
-    def __init__(self, title, poster_image_url, trailer_youtube_url):
-        self.title = title
+    def __init__(self):
+        self.title = ""
+        self.poster_image_url = ""
+        self.trailer_youtube_url = ""
+
+    def set_title(self, new_title):
+        self.title = new_title
+        return self
+
+    def set_poster_image_url(self, poster_image_url):
         self.poster_image_url = poster_image_url
-        self.trailer_youtube_url = trailer_youtube_url
+        return self
+
+    def set_trailer_youtube_url(self, youtube_url):
+        self.trailer_youtube_url = youtube_url
+        return self
+
+    def is_valid(self):
+        result = True
+        if not self.is_valid_string(self.title):
+            result = False
+        if not self.is_valid_string(self.poster_image_url):
+            result = False
+        if not self.is_valid_string(self.trailer_youtube_url):
+            result = False
+
+        return result
+
+    def is_valid_string(self, string):
+        return (type(string) is str) and (len(string) > 0)
+
+    def get_movie(self):
+        if self.is_valid():
+            return Movie(self)
+
+class Movie():
+    
+    def __init__(self, movie_essence):
+        self.title = movie_essence.title
+        self.poster_image_url = movie_essence.poster_image_url
+        self.trailer_youtube_url = movie_essence.trailer_youtube_url
 
