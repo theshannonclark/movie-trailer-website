8a995980d7728a7922b18d68ecda80b28cf2b9ab
Shannon Clark <theshannonclark@gmail.com>
2015-12-03 16:24:37 +1100

    Create MovieEssence class
    The Essence design pattern encapsulates the configuration of the target object. I'm
    trying to add properties to the movie class without giving the Movie constructor
    an enormous parameter list.

--- media.py
+++ media.py
@@ -1,9 +1,25 @@
 import lib.fresh_tomatoes as ft
 
-class Movie():
+class MovieEssence():
 
-    def __init__(self, title, poster_image_url, trailer_youtube_url):
-        self.title = title
+    def __init__(self):
+        self.title = ""
+        self.poster_image_url = ""
+        self.trailer_youtube_url = ""
+
+    def set_titles(new_title):
+        self.title = new_title
+
+    def set_poster_image_url(poster_image_url):
         self.poster_image_url = poster_image_url
-        self.trailer_youtube_url = trailer_youtube_url
+
+    def set_trailer_youtube_url(youtube_url):
+        self.trailer_youtube_url = youtube_url
+
+class Movie():
+    
+    def __init__(self, movie_essence):
+        self.title = movie_essence.title
+        self.poster_image_url = movie_essence.poster_image_url
+        self.trailer_youtube_url = movie_essence.trailer_youtube_url
 
